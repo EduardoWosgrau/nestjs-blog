265554bb6329ea344d24c389041ec8b2f12cddb6
Eduardo Wosgrau <eduardo.wosgrau@gmail.com>
2024-10-01 11:04:28 -0300

    auto load entities TypeORM - post, tags and metaOptions

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { PostsController } from './posts.controller';
 import { UsersModule } from 'src/users/users.module';
+import { Post } from './post.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forFeature([Post])],
 })
 export class PostsModule {}
